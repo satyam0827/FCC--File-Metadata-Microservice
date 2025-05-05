d1754311761605ddde376d59121f8b395e87c8f9
Satyam kumar <jhajhasatyam100@gmail.com>
2025-05-06 00:29:59 +0530

    challenge completed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,11 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/upload",(req,res)=>{
-  
-})
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
